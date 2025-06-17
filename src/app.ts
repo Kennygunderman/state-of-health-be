3e6f387c1effb2c87d0cac20ab106ff7e49683e8
kennygunderman <kennygunderman@gmail.com>
2025-06-17 12:04:43 -0400

    Add migration endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import workoutRoutes from './routes/workout.routes';
+import migrationRoutes from './routes/migration.routes';
 
 const app = express();
 app.use(express.json());
 app.use('/api', workoutRoutes);
+// app.use('/api', migrationRoutes);
 export default app;
